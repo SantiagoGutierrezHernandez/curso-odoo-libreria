8b2f8f22d2ec65300ee6fae8061eb093af7d0a40
Santiago Gutierrez Hernandez <santiagogutierrezhernandez1012@gmail.com>
2023-09-07 13:32:22 -0300

    library module manifest

---
+++ src/user/library/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Library",
+    "summary": "Módulo pare gestionar la organización y alquiler de libros.",
+    "description": """Módulo para gestionar:
+        - Clientes
+        - Libros
+    """,
+    "license": "OPL-1",
+    "author": "SantiagoGutierrezHernandez",
+    "website": "https://github.com/SantiagoGutierrezHernandez/curso-odoo-libreria",
+    "category": "Custom Modules/Library",
+    "depends": ["base"],
+    "data": [],
+    "demo": [],
+    "application": True,
+}
