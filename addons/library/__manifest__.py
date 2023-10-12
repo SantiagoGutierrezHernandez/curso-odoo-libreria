ac80514b1af309389454725a3806615a3d2fe059
Santiago Gutierrez Hernandez <santiagogutierrezhernandez1012@gmail.com>
2023-10-12 13:44:32 -0300

    library groups

--- addons/library/__manifest__.py
+++ addons/library/__manifest__.py
@@ -10,7 +10,12 @@
     "website": "https://github.com/SantiagoGutierrezHernandez/curso-odoo-libreria",
     "category": "Custom Modules/Library",
     "depends": ["base"],
-    "data": [],
+    "data": [
+        "security/library_groups.xml",
+        "security/ir.model.access.csv",
+        "views/book_list_action.xml",
+        "views/book_menuitem.xml",
+    ],
     "demo": [
         "demo/book_demo.xml"
     ],
